5888784a1075aaa49069c7812f5dab9e513d18f2
csaben <76020733+csaben@users.noreply.github.com>
2020-12-24 16:04:03 -0500

    Update initial.py

--- initial.py
+++ initial.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+import matplotlib as plt
+from IPython.display import clear_output
+from six.moves import urllib_error
+
 
 #This will act as standalone file that will be be then merged with the main as a sort of internal code test repository
 
